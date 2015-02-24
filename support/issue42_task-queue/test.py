6c1f76aedc7b65184ec34fa481ce9fd6ded98082
Joshua Johnston <johnston.joshua@gmail.com>
2015-02-24 16:41:33 -0500

    0.5.3 Release
    * Add Travis-CI testing for all items under support
    * Bugfix for automatic taskqueue initialization

--- support/issue42_task-queue/test.py
+++ support/issue42_task-queue/test.py
@@ -1,4 +1,5 @@
 from webtest import TestApp
+import unittest
 from helloworld import app
 import os
 
@@ -18,3 +19,12 @@ test_index.nosegae_taskqueue = True
 
 # ...or you can manually set the path and any additional arguments with the kwargs attribute
 test_index.nosegae_taskqueue_kwargs = dict(task_retry_seconds=42, root_path=os.path.dirname(__file__))
+
+
+class TestIndex(unittest.TestCase):
+    nosegae_taskqueue = True
+
+    def test_index(self):
+        response = app.get('/')
+        self.assertIn('Hello world!', response.body)
+        self.assertEqual(response.status_int, 200)
