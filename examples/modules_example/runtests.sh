6cf46db090cf813c49e6e8289dadd2c622969803
Joshua Johnston <johnston.joshua@gmail.com>
2015-10-05 12:40:41 -0400

    Switch tests / travis to use app engine sdk zip
    gcloud no longer provides the sdk so we have to pull it manually

--- examples/modules_example/runtests.sh
+++ examples/modules_example/runtests.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-nosetests --with-gae --gae-lib-root ../google-cloud-sdk/platform/google_appengine \
+nosetests --with-gae --gae-lib-root ../google_appengine \
           --gae-application='app.yaml,mobile_frontend.yaml,static_backend.yaml,dispatch.yaml'
--- examples/tests_in_package/runtests.sh
+++ examples/tests_in_package/runtests.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 cd "$(cd -P -- "$(dirname -- "$0")" && pwd -P)"
-nosetests --with-gae --gae-lib-root ../google-cloud-sdk/platform/google_appengine;
+nosetests --with-gae --gae-lib-root ../google_appengine;
